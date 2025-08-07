f4a1215fc8c5491624e7915e8fb5f9d3982b8ae4
redshiftzero <jen@relicveil.com>
2025-08-07 11:19:25 -0400

    protocol: module skeleton for signing primitive

---
+++ securedrop-protocol/src/sign.rs
@@ -0,0 +1,33 @@
+use rand_core::{CryptoRng, RngCore};
+
+// TODO(jen): Replace with libcrux Ed25519 types
+
+/// A signing key.
+#[derive(Copy, Clone)]
+pub struct SigningKey {
+    vk: VerifyingKey,
+}
+
+#[derive(Copy, Clone)]
+pub struct VerifyingKey {}
+
+pub struct Signature {}
+
+impl SigningKey {
+    /// Generate a signing key from the supplied `rng`.
+    pub fn new<R: RngCore + CryptoRng>(mut rng: R) -> SigningKey {
+        unimplemented!()
+    }
+
+    /// Create a signature on `msg` using this `SigningKey`.
+    pub fn sign<R: RngCore + CryptoRng>(&self, mut rng: R, msg: &[u8]) -> Signature {
+        unimplemented!()
+    }
+}
+
+impl VerifyingKey {
+    /// Verify a signature on `msg` using this `VerifyingKey`
+    pub fn verify(&self, msg: &[u8], signature: &Signature) -> Result<(), Error> {
+        unimplemented!()
+    }
+}
