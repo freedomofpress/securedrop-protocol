44ab89435a0f9190589ba98700a6c18d89f89dd1
redshiftzero <jen@relicveil.com>
2025-08-07 12:38:27 -0400

    python: rename keys dir so we can have a `keys` module in Rust

--- deploy_keys.py
+++ deploy_keys.py
@@ -1,15 +1,15 @@
 import requests
 import commons
 
-with open("keys/root.public", "r") as f:
+with open("cryptokeys/root.public", "r") as f:
     fpf_key = f.read()
 
-with open("keys/intermediate.public", "r") as f:
+with open("cryptokeys/intermediate.public", "r") as f:
     nr_key = f.read()
 
-with open("keys/intermediate.sig", "r") as f:
+with open("cryptokeys/intermediate.sig", "r") as f:
     nr_sig = f.read()
 
-res = requests.post(f"http://{commons.SERVER}/keys", json={"fpf_key": fpf_key, "newsroom_key": nr_key, "newsroom_sig": nr_sig})
+res = requests.post(f"http://{commons.SERVER}/cryptokeys", json={"fpf_key": fpf_key, "newsroom_key": nr_key, "newsroom_sig": nr_sig})
 
 assert(res.status_code == 200)
