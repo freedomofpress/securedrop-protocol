c7dfef069ead0bd89f978f54e31f15cee8ed8347
redshiftzero <jen@relicveil.com>
2025-08-25 19:33:54 -0400

    protocol: drop in libcrux_curve25519 for DH keys

--- securedrop-protocol/src/primitives.rs
+++ securedrop-protocol/src/primitives.rs
@@ -1,6 +1,7 @@
-// TODO(ro): Import whatever crate provides the appropriate keys
-
-// TODO(ro): Fill in types here
+use anyhow::Error;
+use libcrux_curve25519::{DK_LEN as SK_LEN, EK_LEN as PK_LEN};
+use libcrux_traits::kem::arrayref::Kem;
+use rand_core::{CryptoRng, RngCore};
 
 #[derive(Debug, Clone)]
 pub struct PPKPrivateKey;
@@ -16,14 +17,45 @@ impl PPKPublicKey {
 }
 
 #[derive(Debug, Clone)]
-pub struct DHPublicKey;
+pub struct DHPublicKey([u8; PK_LEN]);
 
 impl DHPublicKey {
-    pub fn into_bytes(self) -> [u8; 32] {
-        // TODO: Implement when actual DH types are available
-        [0u8; 32]
+    pub fn into_bytes(self) -> [u8; PK_LEN] {
+        self.0
+    }
+
+    pub fn from_bytes(bytes: [u8; PK_LEN]) -> Self {
+        Self(bytes)
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct DHPrivateKey;
+pub struct DHPrivateKey([u8; SK_LEN]);
+
+impl DHPrivateKey {
+    pub fn into_bytes(self) -> [u8; SK_LEN] {
+        self.0
+    }
+
+    pub fn from_bytes(bytes: [u8; SK_LEN]) -> Self {
+        Self(bytes)
+    }
+}
+
+/// Generate a new DH key pair using X25519
+pub fn generate_dh_keypair<R: RngCore + CryptoRng>(
+    mut rng: R,
+) -> Result<(DHPrivateKey, DHPublicKey), Error> {
+    let mut randomness = [0u8; 32];
+    rng.fill_bytes(&mut randomness);
+
+    let mut public_key = [0u8; PK_LEN];
+    let mut secret_key = [0u8; SK_LEN];
+
+    // Generate the key pair using X25519 from libcrux
+    // Parameters: ek (public key), dk (secret key), rand (randomness)
+    libcrux_curve25519::X25519::keygen(&mut public_key, &mut secret_key, &randomness)
+        .map_err(|_| anyhow::anyhow!("X25519 key generation failed"))?;
+
+    Ok((DHPrivateKey(secret_key), DHPublicKey(public_key)))
+}
