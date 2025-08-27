ca526011630a4fc7b28be870c211ac5575ff8e62
Rowen S <ro@freedom.press>
2025-08-27 13:59:40 -0400

    WIP: Add Message and Metadata component struts, fix message response type

--- securedrop-protocol/src/messages/core.rs
+++ securedrop-protocol/src/messages/core.rs
@@ -2,8 +2,56 @@ use crate::primitives::{DHPublicKey, PPKPublicKey};
 use crate::{Signature, VerifyingKey};
 use alloc::vec::Vec;
 
-/// TODO: Should be (C, Z, X)
-pub struct MessageBundle {}
+pub struct MessageBundle {
+    /// C
+    pub sealed_envelope: SealedEnvelope,
+    /// (Z, X)
+    pub message_clue: MessageClue,
+}
+
+pub struct SealedEnvelope {
+    /// HPKE AuthPSK Sealed ciphertext
+    pub sealed_message: SealedMessage,
+
+    /// HPKE BaseMode Sealed X-WING encapsulated metadata (contains
+    /// sender pubkey needed to open SealedMessage.
+    /// See https://www.rfc-editor.org/rfc/rfc9180#section-9.9)
+    pub sealed_metadata: SealedMessageMetadata,
+    /// X-WING shared secret encaps, 1120 bytes
+    pub metadata_encaps: Vec<u16>,
+}
+
+/// Sealed metadata bytes
+pub struct SealedMessageMetadata {}
+
+/// Metadata for decrypting ciphertext
+/// TODO: check int sizes
+pub struct MessageMetadata {
+    pub sender_key: DHPublicKey,
+    message_dhakem_secret_encaps: Vec<u8>,
+    message_psk_secret_encaps: Vec<u8>,
+}
+
+/// Ciphertext bytes
+pub struct SealedMessage {}
+
+/// TODO: Plaintext message structure (i.e what keys or hashes of keys are included?)
+/// At minimum:
+/// Sender XWING key (for replies metadata)
+/// Sender MLKEM key (for replies)
+/// Identifiers: newsroom identifier
+/// Fetching key identifier?
+/// DH-AKEM key identifier (maybe not needed bc of how auth mode in hpke works)?
+/// Plaintext
+pub struct Message {}
+
+/// (Z, X)
+pub struct MessageClue {
+    /// DH share
+    pub clue_bytes: Vec<u8>,
+    /// Ephemeral DH pubkey
+    pub clue_pubkey: DHPublicKey,
+}
 
 /// Source fetches keys for the newsroom
 ///
@@ -52,27 +100,25 @@ pub struct SourceJournalistKeyResponse {
 /// User submits a message to the server $(C, Z, X)$
 ///
 /// This corresponds to step 6 for sources and step 9 for journalists in the spec.
-pub struct MessageSubmitRequest {
-    /// Encrypted message ciphertext
-    ciphertext: Vec<u8>,
-    /// Diffie-Hellman share Z
-    dh_share_z: Vec<u8>,
-    /// Diffie-Hellman share X
-    dh_share_x: Vec<u8>,
-}
+/// TODO: could remove this, just kept it for consistency of naming with the other types
+pub type MessageSubmitRequest = MessageBundle;
 
 /// User (source or journalist) fetches message IDs
 ///
 /// This corresponds to step 7 in the spec.
+/// TODO: "FetchRequest/ChallengeFetchRequest"
 pub struct MessageIdFetchRequest {}
 
 /// Server returns encrypted message IDs
 ///
 /// This corresponds to step 7 in the spec.
+/// TODO: naming as above?
 pub struct MessageIdFetchResponse {
     /// Number of message entries returned
+    /// TODO: constant size response
     count: usize,
     /// Array of (Q, cid) pairs where Q is the group DH share and cid is encrypted message ID
+    /// TODO: constant size array
     messages: Vec<(Vec<u8>, Vec<u8>)>,
 }
 
@@ -87,9 +133,5 @@ pub struct MessageFetchRequest {
 /// Server returns the requested message
 ///
 /// This corresponds to step 8 and 10 in the spec.
-pub struct MessageFetchResponse {
-    /// Encrypted message ciphertext
-    ciphertext: Vec<u8>,
-    /// Diffie-Hellman share X (only for source→journalist messages)
-    dh_share_x: Option<Vec<u8>>,
-}
+/// TODO: may remove alias
+pub type MessageFetchResponse = MessageBundle;
