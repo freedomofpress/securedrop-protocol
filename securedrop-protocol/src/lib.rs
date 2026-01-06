adb1f4ca3ad32d20639c808c5b6525b32f9054f1
Rowen S <ro@freedom.press>
2026-01-06 16:20:32 -0500

    wip: build plaintext in encrypt_once

--- securedrop-protocol/src/lib.rs
+++ securedrop-protocol/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
-
 extern crate alloc;
+extern crate console_error_panic_hook;
 
 pub mod client;
 pub mod keys;
@@ -55,7 +55,7 @@ impl WSource {
         WSource {
             inner: Source::new(&mut rng),
         }
-    }
+    }    
 }
 
 #[wasm_bindgen]
@@ -137,17 +137,41 @@ pub fn encrypt_once(
     sender: &WSource,
     recipient: &WJournalist,
     recipient_bundle_index: usize,
-    plaintext: &[u8],
+    msg: &[u8],
 ) -> WEnvelope {
+
+    use crate::bench::encrypt_decrypt::{LEN_MLKEM_ENCAPS_KEY,LEN_DH_ITEM,LEN_XWING_ENCAPS_KEY};
+
+    // log errors
+    console_error_panic_hook::set_once();
+
     assert_eq!(seed32.len(), 32, "seed32 must be 32 bytes");
     let mut seed = [0u8; 32];
     seed.copy_from_slice(seed32);
+
+    // build plaintext object
+    let mut pq = [0u8; LEN_MLKEM_ENCAPS_KEY];
+    pq.copy_from_slice(&sender.inner.keys.pq_kem_psk_pk);
+
+    let mut hybrid = [0u8; LEN_XWING_ENCAPS_KEY];
+    hybrid.copy_from_slice(&sender.inner.keys.hybrid_md_pk);
+
+    let mut fetch = [0u8; LEN_DH_ITEM];
+    fetch.copy_from_slice(sender.inner.get_fetch_pk());
+
+    let plaintext = Plaintext {
+        sender_reply_pubkey_pq_psk: pq,
+        sender_reply_pubkey_hybrid: hybrid,
+        sender_fetch_key: fetch,
+        msg: msg.to_vec(),
+    };
+
     let env = bench_encrypt(
         seed,
         &sender.inner,
         &recipient.inner,
         recipient_bundle_index,
-        plaintext,
+        &plaintext.to_bytes(),
     );
     env.into()
 }
@@ -155,6 +179,9 @@ pub fn encrypt_once(
 /// Returns plaintext bytes.
 #[wasm_bindgen]
 pub fn decrypt_once(recipient: &WJournalist, envelope: &WEnvelope) -> Vec<u8> {
+    // log errors
+    console_error_panic_hook::set_once();
+
     let pt: Plaintext = bench_decrypt(&recipient.inner, &envelope.inner);
     pt.to_bytes()
 }
@@ -166,6 +193,9 @@ pub fn compute_fetch_challenges_once(
     entries: Box<[WStoreEntry]>,
     total_responses: usize,
 ) -> Box<[WFetchResponse]> {
+    // log errors
+    console_error_panic_hook::set_once();
+
     assert_eq!(seed32.len(), 32);
     let mut seed = [0u8; 32];
     seed.copy_from_slice(seed32);
