629d6f320e5a60a6496bbf0f4e97137981fd2159
rowen s. <ro@freedom.press>
2026-05-12 14:59:38 -0400

    refactor: improve sealed trait definition for restricted APIs

---
+++ securedrop-protocol/protocol-minimal/src/sealed.rs
@@ -0,0 +1,9 @@
+/// Use [sealed trait pattern](https://rust-lang.github.io/api-guidelines/future-proofing.html#c-sealed)
+/// to gate features that downstream crates should not implement.
+/// This module should never be exposed publicly or re-exported (i.e., no `pub use::sealed::*`)!
+/// To use, define a public trait (eg in traits.rs):
+/// ``` use crate::sealed; ```
+/// ``` pub trait RestrictedTrait: sealed::Sealed {}` ```
+/// The `Sealed` trait is public as long as UserSecret, RestrictedApi, etc are public;
+/// if those ever become pub(crate) then this trait can be restricted as well.
+pub trait Sealed {}
