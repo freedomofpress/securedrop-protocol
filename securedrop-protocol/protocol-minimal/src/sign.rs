692328742f69a1d79b5e55a33a623d84d2b73e12
redshiftzero <jen@relicveil.com>
2026-06-11 14:11:37 -0400

    protocol: add `as_bytes`,`from_seed` for `SigningKey`

--- securedrop-protocol/protocol-minimal/src/sign.rs
+++ securedrop-protocol/protocol-minimal/src/sign.rs
@@ -151,6 +151,20 @@ impl SigningKey {
             .expect("Signing should not fail with valid key");
         Signature::from_bytes(bytes)
     }
+
+    pub(crate) fn as_bytes(&self) -> [u8; 32] {
+        *self.sk.as_ref()
+    }
+
+    pub(crate) fn from_seed(seed: [u8; 32]) -> Self {
+        let sk = LibCruxSigningKey::from_bytes(seed);
+        let mut pk = [0u8; 32];
+        provider::ed25519::secret_to_public(&mut pk, sk.as_ref());
+        Self {
+            vk: VerifyingKey(LibCruxVerifyingKey::from_bytes(pk)),
+            sk,
+        }
+    }
 }
 
 impl VerifyingKey {
