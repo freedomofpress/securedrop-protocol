eeed9f81323208b3c0c307fde9b57f1d5613f9c7
redshiftzero <jen@relicveil.com>
2026-07-28 10:38:57 -0400

    ristretto: define public, private keys based on ristretto
    keeping the same naming to make this a drop-in diff where we should
    just need to change import paths

--- securedrop-protocol/protocol-minimal/src/primitives/ristretto255.rs
+++ securedrop-protocol/protocol-minimal/src/primitives/ristretto255.rs
@@ -1,7 +1,18 @@
-//! Diffie-Hellman over ristretto255 [RFC 9496]
+//! Diffie-Hellman over ristretto255 [RFC 9496](https://www.rfc-editor.org/rfc/rfc9496)
 
 use crate::primitives::provider;
 
 pub const DH_PUBLIC_KEY_LEN: usize = provider::ristretto255::PK_LEN;
 pub(crate) const DH_PRIVATE_KEY_LEN: usize = provider::ristretto255::SK_LEN;
 pub(crate) const DH_SHARED_SECRET_LEN: usize = provider::ristretto255::LEN_DH_SHARE;
+
+/// Uniform bytes required to derive a scalar per [RFC 9496] section 4.4.
+pub const DH_SEED_LEN: usize = provider::ristretto255::SEED_LEN;
+
+/// A compressed ristretto255 group element.
+#[derive(Debug, Clone, Copy)]
+pub struct DHPublicKey([u8; DH_PUBLIC_KEY_LEN]);
+
+/// A ristretto255 scalar in $\mathbb{Z}_\ell$ canonically encoded.
+#[derive(Debug, Clone)]
+pub struct DHPrivateKey([u8; DH_PRIVATE_KEY_LEN]);
